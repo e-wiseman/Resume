6785300be3bbaf0bc4785b54ca90da7e2a6a2052
Emily Wiseman <ewiseman@mun.ca>
2020-01-20 15:46:33 -0500

    Refactored css and finished main features of Styles page

--- src/js/intro-page.js
+++ src/js/intro-page.js
@@ -1,22 +1,25 @@
 class IntroPage extends React.Component {
+
+    tagline = "A  third year computer engineering student from Newfoundland, Canada. I have 16 months of internship experienace"
+    subline = "I am looking for my next adventure!"
+    
     render() {
         return (
             <div className="container d-md-flex pt-5 pb-5 text-center text-md-left">
                 <div>
-                    <img id="emily" src="/src/assets/emily.svg"/>
+                    <img id="id-photo" src="/src/assets/emily.svg"/>
                 </div>
                 <div className="mt-2 mt-md-0">
                     <h1>Hi, I'm Emily!</h1>
                     <p id="intro" className="text-wrap">
-                        A  third year computer engineering student from Newfoundland, Canada. I have 16 months of internship experienace and 
-                        <strong><i>I am looking for my next adventure!</i></strong>
+                        { this.tagline } and <strong><i>{ this.subline }</i></strong>
                     </p>
                     <nav id="downloads" className="mt-3">
-                        <a className="btn-poppy remove-md-br" href="#">
+                        <a className="btn main-btn remove-md-br" href="#">
                             <i className="pr-1 far fa-envelope"></i>
                             <p>ewiseman@mun.ca</p>
                         </a>
-                        <a className="btn-poppy" href="#">
+                        <a className="btn main-btn" href="#">
                             <i className="pr-1 fas fa-file-download"></i>
                             <p>Download My Resume</p>
                         </a>
--- src/js/skills-page.js
+++ src/js/skills-page.js
@@ -1,14 +1,127 @@
 class SkillsPage extends React.Component {
+    makeTags ( element ){
+        const skills = element.map((skillName) => 
+            <span className="tag light-bg main-text">{ skillName }</span>
+        );
+        return (
+            skills
+        );
+    }
+
+    displayItem ( category ){
+        const item = category.map((skillsCategory) =>
+            <li className="d-flex-inline flex-wrap">
+                <i className="light-text fas fa-arrow-right"></i>
+                <p className="light-text">{ skillsCategory.name }</p>
+                { this.makeTags( skillsCategory.skills ) }
+            </li>
+        );
+
+        return (
+            <ul className="pl-4">{ item }</ul>
+        );
+    }
+
     render() {
         return (
-            <div className="poppy">
-                <div className="container d-md-flex pt-5 pb-5 text-center text-md-left">
-                    <h2 class="poppy">Skills</h2>
-                </div>
-                
-            </div>
+            <div className="main-bg">
+                <div className="container pt-5 pb-5">
+                    <h2 className="light-text">Skills <i className="fas fa-check"></i></h2>
+                    <div className="d-md-flex justify-content-around">
+
+                        <div className="p-3 text-center text-md-left">
+                            <h3 className="light-text">Languages</h3>
+                            { this.displayItem( this.props.data.languages ) }
+
+                            <h3 className="light-text">Soft Skills</h3>
+                            { this.displayItem( this.props.data.softSkills ) }
+                        </div>
+
+                        <div className="p-3 text-center text-md-left">
+                            <h3 className="light-text">Web Development</h3>
+                            { this.displayItem( this.props.data.webDev ) }
+                        </div>
+
+                    </div>
+            </div>        
+        </div>
         )
     }
 }
 
-ReactDOM.render(<SkillsPage />, document.getElementById('skills-page'))
+let SKILLS = {
+    languages: [
+        {
+            name: "Python",
+            skills: [ "Work Experience", "Intermediate", "Projects" ]
+        },
+        {
+            name: "Java",
+            skills: [ "School", "Projects" ]
+        },
+        {
+            name: "C/C++",
+            skills: [ "School" ]
+        },
+        {
+            name: "Bash Scripting",
+            skills: [ "Work Experience", "Beginer" ]
+        },
+        {
+            name: "Shell Scripting",
+            skills: [ "Work Experience", "Intermediate" ]
+        }
+    ],
+
+    webDev: [
+        {
+            name: "HTML & CSS",
+            skills: [ "Work Experience", "Intermediate", "Projects" ]
+        },
+        {
+            name: "Java Script & Type Script",
+            skills: [ "Work Experience", "Projects" ]
+        },
+        {
+            name: "Django",
+            skills: [ "Work Experience" ]
+        },
+        {
+            name: "Flask & HTTP Requests",
+            skills: [ "Work Experience", "Intermediate" ]
+        },
+        {
+            name: "Angular",
+            skills: [ "Work Experience" ]
+        },
+        {
+            name: "Vue.js",
+            skills: [ "Work Experience" ]
+        },
+        {
+            name: "React & React Native",
+            skills: [ "Projects" ]
+        },
+        {
+            name: "Firebase, Postgress, & MySQL databases",
+            skills: [ "Work Experience", "Projects" ]
+        }
+    ],
+
+    softSkills: [
+        {
+            name: "Experience working in Agile development",
+            skills: [ "Work Experience" ]
+        },
+        {
+            name: "Experience using JIRA & Bitbucket",
+            skills: [ "Work Experience", "Intermediate" ]
+        },
+        {
+            name: "Experience making & leading meetings",
+            skills: [ "Work Experience" ]
+        }
+    ]
+}
+
+ReactDOM.render(<SkillsPage data={SKILLS} />, document.getElementById('skills-page'))
